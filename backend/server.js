689711311b3b5fbeaacfe6613ea1d3aec496a9c8
rafboah <rafboah@gmail.com>
2024-04-20 19:28:05 -0400

    Added .env, .gitignore, and node_modules/dotenv/

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
+require('dotenv').config();
 const express = require('express');
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3001;
 
 app.get('/', (req, res) => {
     res.send('');
