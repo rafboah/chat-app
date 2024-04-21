62303fea85b7663362d865b48361269883ef65af
rafboah <rafboah@gmail.com>
2024-04-21 01:03:29 -0400

    Setup real time messaging with socket.io for backend and socket.io-client for frontend

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,26 @@
 require('dotenv').config();
 
 const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
 
 // const test = require('./server.js')
 const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+io.on('connection', (socket) =>{
+    console.log('Client connected');
+
+    socket.on('disconnection', () => {
+        console.log('Client disconnected');
+    });
+
+    socket.on('message', (message) => {
+        console.log('Message received: ', message);
+        io.emit('message', message);
+    });
+});
 
 const PORT = process.env.PORT || 3001;
 
@@ -11,8 +28,7 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-app.listen(PORT, () => {
-    console.log('Port from .env', process.env.PORT);
+server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
 
