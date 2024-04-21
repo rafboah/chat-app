dca831972dd006e3f7e9795edf414bbb22d69b27
rafboah <rafboah@gmail.com>
2024-04-20 20:43:28 -0400

    Added nodemon.json config file and app.test.js

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,19 @@
 require('dotenv').config();
+
 const express = require('express');
 
+// const test = require('./server.js')
 const app = express();
 
 const PORT = process.env.PORT || 3001;
 
 app.get('/', (req, res) => {
-    res.send('');
+    res.send('Hello World!');
 });
 
 app.listen(PORT, () => {
+    console.log('Port from .env', process.env.PORT);
     console.log(`Server running on port ${PORT}`);
 });
+
+// module.exports = test;
